45375975a0648c01b5616f3ed6912ad270f3f563
nickzylstra <nick.zylstra@gmail.com>
2019-11-04 17:31:59 -0800

    Refactors app to es6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,35 @@ import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      videoList: exampleVideoData,
+      currentVideo: exampleVideoData[0]
+    };
+  }
+
+  render () {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><em>videoPlayer</em></h5><VideoPlayer video={this.state.currentVideo} /></div>
+          </div>
+          <div className="col-md-5">
+            <div><h5><em>videoList</em></h5><VideoList videos={this.state.videoList}/></div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em></h5><VideoPlayer video={exampleVideoData[0]} /></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em></h5><VideoList videos={exampleVideoData}/></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
