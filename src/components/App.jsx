6582f8645bc921f8678da015d9971138085f2a35
nickzylstra <nick.zylstra@gmail.com>
2019-11-04 17:10:41 -0800

    Adds video player functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
 var App = () => (
@@ -10,10 +11,10 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em>videoPlayer</em></h5><VideoPlayer video={exampleVideoData[0]} /></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em><VideoList videos={exampleVideoData}/></h5></div>
+        <div><h5><em>videoList</em></h5><VideoList videos={exampleVideoData}/></div>
       </div>
     </div>
   </div>
